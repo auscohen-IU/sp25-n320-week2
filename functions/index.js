82cc93226e7b03f03377d8cf1d660ad2365dff5b
auscohen-IU <auscohen@iu.edu>
2025-01-21 15:37:51 -0500

    We stopped for a little to do some group work, i'm going to see if I can catch up a little more.

---
+++ functions/index.js
@@ -0,0 +1,11 @@
+function calcTriArea(base, height){
+    return 0.5 * base * height;
+}
+
+function calcSquareArea(length, width){
+    return length * width;
+}
+
+module.exports = {
+    calcTriArea, calcSquareArea
+};
---
+++ index.js
@@ -0,0 +1,7 @@
+const { calcTriArea } = require("./functions");
+
+const { calcTriArea } = mathFuncs;
+
+console.log(calcTriArea({base: 3, height: 7}));
+
+console.log (calcTriArea({base: 2}));
